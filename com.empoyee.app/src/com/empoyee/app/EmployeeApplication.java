0f5f98c03d4bfdde98a52312450f5e55cce4a39a
pradip shinde <pradipshinde013@gmail.com>
2025-02-16 18:00:01 +0530

    Initial Commit

---
+++ com.empoyee.app/src/com/empoyee/app/EmployeeApplication.java
@@ -0,0 +1,9 @@
+package com.empoyee.app;
+
+public class EmployeeApplication {
+	public static void main(String[] args) {
+		System.out.println("Hello  welcome to the main......");
+		System.out.println("======================== :Employee Application V1.0.0: ========================");
+	}
+
+}
